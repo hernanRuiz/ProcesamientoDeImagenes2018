8325abb4a0127bb2800498dd758c20a7b0a48c39
Hernan <Hernan@NB_HERNAN>
2015-04-11 01:11:35 -0300

    Intento de detector de bordes de Roberts, usando la clase filtro para pasar las mascaras y Math.hypot para el ultimo paso de raiz (x^2 + y^2). Quizas al elevar al cuadrado haya que meter transformada logarítmica pero después no se como hacer la raiz.

--- src/ar/com/untref/imagenes/bordes/DetectorDeBordes.java
+++ src/ar/com/untref/imagenes/bordes/DetectorDeBordes.java
@@ -1,28 +1,87 @@
 package ar.com.untref.imagenes.bordes;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.Kernel;
+
+import ar.com.untref.imagenes.filtros.Filtro;
+import ar.com.untref.imagenes.modelo.Imagen;
+import ar.com.untref.imagenes.procesamiento.ProcesadorDeImagenes;
 
 public class DetectorDeBordes {
 
-	public  void aplicarDetectorDeRoberts(BufferedImage bufferedImage){
+	public static BufferedImage aplicarDetectorDeRoberts(Imagen imagenOriginal){
 		
-		int[][] mascaraDeRobertsEnX = new int [2][2];
+		float[][] mascaraDeRobertsEnX = new float [2][2];
 		mascaraDeRobertsEnX[0][0]= 1;
 		mascaraDeRobertsEnX[0][1]= 0;
 		mascaraDeRobertsEnX[1][0]= 0;
 		mascaraDeRobertsEnX[1][1]= -1;
 		
-		int[][] mascaraDeRobertsEnY = new int [2][2];
+		float[][] mascaraDeRobertsEnY = new float [2][2];
 		mascaraDeRobertsEnY[0][0]= 0;
 		mascaraDeRobertsEnY[0][1]= 1;
 		mascaraDeRobertsEnY[1][0]= -1;
 		mascaraDeRobertsEnY[1][1]= 0;
 		
+		BufferedImage im = new BufferedImage(imagenOriginal.getBufferedImage().getWidth(), imagenOriginal.getBufferedImage().getHeight(), imagenOriginal.getBufferedImage().getType());
+		Imagen imagenFiltradaEnX = new Imagen(im, imagenOriginal.getFormato(), imagenOriginal.getNombre());
+		Imagen imagenFiltradaEnY = new Imagen(im, imagenOriginal.getFormato(), imagenOriginal.getNombre());
+		Imagen imagenResultante = new Imagen(im, imagenOriginal.getFormato(), imagenOriginal.getNombre());
+		
+		int xPixel = 0;
+		int yPixel = 0;
+		int ancho1 = mascaraDeRobertsEnX.length;
+        int alto1 = mascaraDeRobertsEnX[0].length;
+        int tam1 = ancho1 * alto1;
+        float filtroK1[] = new float[tam1];
+        
+    	int ancho2 = mascaraDeRobertsEnY.length;
+        int alto2 = mascaraDeRobertsEnY[0].length;
+        int tam2 = ancho2 * alto2;
+        float filtroK2[] = new float[tam2];
+        
+      //Creamos el filtro - Se pasa de una matriz cuadrada (vector de 2 dimensiones) a un vector lineal
+        for(int i=0; i < ancho1; i++){
+            for(int j=0; j < alto1; j++){
+                filtroK1[i*ancho1 + j] = mascaraDeRobertsEnX[i][j];
+            }
+        }
+        
+      //Creamos el filtro - Se pasa de una matriz cuadrada (vector de 2 dimensiones) a un vector lineal
+        for(int i=0; i < ancho2; i++){
+            for(int j=0; j < alto2; j++){
+                filtroK2[i*ancho2 + j] = mascaraDeRobertsEnY[i][j];
+            }
+        }
+        
+        Kernel kernelX = new Kernel(ancho1, alto1, filtroK1);
+        Filtro filtroX = new Filtro(kernelX);
+        
+        Kernel kernelY = new Kernel(ancho2, alto2, filtroK2);
+        Filtro filtroY = new Filtro(kernelY);
+        
+        //Aplicamos filtros
+        filtroX.filter(imagenOriginal.getBufferedImage(), imagenFiltradaEnX.getBufferedImage());
+        filtroY.filter(imagenOriginal.getBufferedImage(), imagenFiltradaEnY.getBufferedImage());
+        		
+        		
+        //Creamos la imagen resultante
+        for (int i = 0; i < ancho1; i++) {
+            for (int j = 0; j < alto1; j++) {
+                xPixel = imagenFiltradaEnX.getBufferedImage().getRGB(i, j);
+                yPixel = imagenFiltradaEnY.getBufferedImage().getRGB(i, j);
+                imagenResultante.getBufferedImage().setRGB(i, j, (int)Math.hypot(xPixel, yPixel));
+            }
+        }
+        
+        return imagenResultante.getBufferedImage();
+        
+		
 	}
 	
 	public  void aplicarDetectorDePrewitt(BufferedImage bufferedImage){
 		
-		int[][] mascaraDePrewittEnX = new int [3][3];
+		float[][] mascaraDePrewittEnX = new float [3][3];
 		mascaraDePrewittEnX[0][0]= -1;
 		mascaraDePrewittEnX[0][1]= -1;
 		mascaraDePrewittEnX[0][2]= -1;
@@ -33,7 +92,7 @@ public class DetectorDeBordes {
 		mascaraDePrewittEnX[2][1]= 1;
 		mascaraDePrewittEnX[2][2]= 1;
 		
-		int[][] mascaraDePrewittEnY = new int [3][3];
+		float[][] mascaraDePrewittEnY = new float [3][3];
 		mascaraDePrewittEnY[0][0]= -1;
 		mascaraDePrewittEnY[0][1]= 0;
 		mascaraDePrewittEnY[0][2]= 1;
@@ -48,7 +107,7 @@ public class DetectorDeBordes {
 	
 	public  void aplicarDetectorDeSobel(BufferedImage bufferedImage){
 		
-		int[][] mascaraDeSobelEnX = new int [3][3];
+		float[][] mascaraDeSobelEnX = new float [3][3];
 		mascaraDeSobelEnX[0][0]= -1;
 		mascaraDeSobelEnX[0][1]= -2;
 		mascaraDeSobelEnX[0][2]= -1;
@@ -59,7 +118,7 @@ public class DetectorDeBordes {
 		mascaraDeSobelEnX[2][1]= 2;
 		mascaraDeSobelEnX[2][2]= 1;
 		
-		int[][] mascaraDeSobelEnY = new int [3][3];
+		float[][] mascaraDeSobelEnY = new float [3][3];
 		mascaraDeSobelEnY[0][0]= -1;
 		mascaraDeSobelEnY[0][1]= 0;
 		mascaraDeSobelEnY[0][2]= 1;
@@ -74,7 +133,7 @@ public class DetectorDeBordes {
 	
 	public  void aplicarDetectorLaplaciano(BufferedImage bufferedImage){
 		
-		int[][] mascaraDeLaplaciano = new int [3][3];
+		float[][] mascaraDeLaplaciano = new float [3][3];
 		mascaraDeLaplaciano[0][0]= 0;
 		mascaraDeLaplaciano[0][1]= 1;
 		mascaraDeLaplaciano[0][2]= 0;
