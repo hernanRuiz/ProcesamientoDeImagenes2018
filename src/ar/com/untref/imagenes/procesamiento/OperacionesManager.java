67c4a2eedcf7bc0fdb9f23b31a8cc8e21dcf0eed
fertwo <fdegirmentjis@gmail.com>
2015-04-22 15:12:27 -0300

    restar imagenes ecualizadas

--- src/ar/com/untref/imagenes/procesamiento/OperacionesManager.java
+++ src/ar/com/untref/imagenes/procesamiento/OperacionesManager.java
@@ -9,6 +9,7 @@ import ar.com.untref.imagenes.enums.NivelMensaje;
 import ar.com.untref.imagenes.helpers.DialogsHelper;
 import ar.com.untref.imagenes.modelo.Imagen;
 import ar.com.untref.imagenes.modelo.OperacionMatematica;
+import ar.com.untref.imagenes.ventanas.VentanaHistogramaEcualizado;
 import ar.com.untref.imagenes.ventanas.VentanaPrincipal;
 
 public class OperacionesManager {
@@ -120,6 +121,48 @@ public class OperacionesManager {
 		Imagen nuevaImagenActual = new Imagen(imagenResultante, imagenAnterior.getFormato(), imagenAnterior.getNombre());
 		ProcesadorDeImagenes.obtenerInstancia().setImagenActual(nuevaImagenActual);
 		ventanaPrincipal.refrescarImagen();
+	}
+	
+	public static Imagen restarImagenes(VentanaHistogramaEcualizado ventana, JPanel panel, Imagen primeraImagen,
+			Imagen segundaImagen, OperacionMatematica operacion) {
+		
+		int[][] matriz1Rojos = MatricesManager.calcularMatrizDeLaImagen(primeraImagen.getBufferedImage(), Canal.ROJO);
+		int[][] matriz1Verdes = MatricesManager.calcularMatrizDeLaImagen(primeraImagen.getBufferedImage(), Canal.VERDE);
+		int[][] matriz1Azules = MatricesManager.calcularMatrizDeLaImagen(primeraImagen.getBufferedImage(), Canal.AZUL);
+		
+		int[][] matriz2Rojos = MatricesManager.calcularMatrizDeLaImagen(segundaImagen.getBufferedImage(), Canal.ROJO);
+		int[][] matriz2Verdes = MatricesManager.calcularMatrizDeLaImagen(segundaImagen.getBufferedImage(), Canal.VERDE);
+		int[][] matriz2Azules = MatricesManager.calcularMatrizDeLaImagen(segundaImagen.getBufferedImage(), Canal.AZUL);
+		
+		int cantidadFilasMatriz1 = matriz1Rojos.length;
+		int cantidadColumnasMatriz1 = matriz1Rojos[0].length;
+		int cantidadFilasMatriz2 = matriz2Rojos.length;
+		int cantidadColumnasMatriz2 = matriz2Rojos[0].length;
+		
+		int[][] matrizResultanteRojos = new int[cantidadFilasMatriz1][cantidadColumnasMatriz2];
+		int[][] matrizResultanteVerdes = new int[cantidadFilasMatriz1][cantidadColumnasMatriz2];
+		int[][] matrizResultanteAzules = new int[cantidadFilasMatriz1][cantidadColumnasMatriz2];
+		
+		if (cantidadFilasMatriz1 == cantidadFilasMatriz2 && cantidadColumnasMatriz1 == cantidadColumnasMatriz2) {
+			
+			matrizResultanteRojos = MatricesManager.aplicarTransformacionLineal(MatricesManager.restarMatrices(matriz1Rojos, matriz2Rojos));
+			matrizResultanteVerdes = MatricesManager.aplicarTransformacionLineal(MatricesManager.restarMatrices(matriz1Verdes, matriz2Verdes));
+			matrizResultanteAzules = MatricesManager.aplicarTransformacionLineal(MatricesManager.restarMatrices(matriz1Azules, matriz2Azules));
+	
+			Imagen imagenAnterior = ProcesadorDeImagenes.obtenerInstancia().getImagenActual();
+			BufferedImage imagenResultante = MatricesManager.generarImagenRGB(matrizResultanteRojos, matrizResultanteVerdes, matrizResultanteAzules);
+			
+			Imagen nuevaImagenActual = new Imagen(imagenResultante, imagenAnterior.getFormato(), imagenAnterior.getNombre());
+			
+			return nuevaImagenActual;
+			
+		} else {
+			
+			DialogsHelper.mostarMensaje(panel, "La "+ operacion.getDescripcion() +" no es posible si las matrices no coinciden en la cantidad de filas y columnas", NivelMensaje.ERROR);
+			ProcesadorDeImagenes.obtenerInstancia().setImagenOriginal(primeraImagen);
+			
+			return null;
+		}
 	}
 	
 }
--- src/ar/com/untref/imagenes/ventanas/VentanaHistogramaEcualizado.java
+++ src/ar/com/untref/imagenes/ventanas/VentanaHistogramaEcualizado.java
@@ -7,11 +7,13 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-import ar.com.untref.imagenes.modelo.HistogramEQ;
-
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -29,19 +31,26 @@ import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import ar.com.untref.imagenes.enums.Canal;
+import ar.com.untref.imagenes.enums.FormatoDeImagen;
+import ar.com.untref.imagenes.enums.NivelMensaje;
+import ar.com.untref.imagenes.helpers.DialogsHelper;
+import ar.com.untref.imagenes.listeners.GuardarComoListener;
 import ar.com.untref.imagenes.listeners.GuardarImagenEcualizadaListener;
+import ar.com.untref.imagenes.modelo.HistogramEQ;
 import ar.com.untref.imagenes.modelo.Imagen;
+import ar.com.untref.imagenes.modelo.OperacionMatematica;
+import ar.com.untref.imagenes.procesamiento.OperacionesManager;
 import ar.com.untref.imagenes.procesamiento.ProcesadorDeImagenes;
 
 @SuppressWarnings("serial")
-public class VentanaHistogramaEcualizado extends JFrame{
+public class VentanaHistogramaEcualizado extends JFrame {
 
 	private JPanel contentPane;
 	private JMenuItem menuItemGuardarComo;
 	private JMenuItem menuItemEcualizar;
 	private BufferedImage bufferedImage;
 	
-	public VentanaHistogramaEcualizado(Imagen imagen) {
+	public VentanaHistogramaEcualizado(final Imagen imagen) {
 		
 		this.setTitle("Ecualización de Histogramas");
 		
@@ -117,6 +126,21 @@ public class VentanaHistogramaEcualizado extends JFrame{
 		menuItemGuardarComo = new JMenuItem("Guardar Como...");
 		menu.add(menuItemGuardarComo);
 		
+		JMenuItem menuItemEcualizarYRestar = new JMenuItem("Ecualizar y restar");
+		menuItemEcualizarYRestar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				Imagen imagenActual = ProcesadorDeImagenes.obtenerInstancia().getImagenActual();
+				if ( imagenActual!=null ){
+					
+					VentanaHistogramaEcualizado ventanaHistogramaEcualizado = new VentanaHistogramaEcualizado(imagenActual);
+					ventanaHistogramaEcualizado.setVisible(true);
+					ventanaHistogramaEcualizado.mostrarResta(imagen, imagenActual);
+				}
+			}
+		});
+		menu.add(menuItemEcualizarYRestar);
+		
 		menu.add(menuItem);
 		
 		for (int i = 0; i < 3; i++) {
@@ -142,6 +166,47 @@ public class VentanaHistogramaEcualizado extends JFrame{
 	
 	}
 	
+	protected void mostrarResta(Imagen imagen, Imagen imagenActual) {
+
+		Imagen imagenDeResta = OperacionesManager.restarImagenes(VentanaHistogramaEcualizado.this, contentPane, imagen, imagenActual, OperacionMatematica.RESTA);
+		guardarImagen(imagenDeResta);
+	}
+
+	private void guardarImagen(Imagen imagenAGuardar) {
+
+		JFileChooser fileChooser = new JFileChooser();
+		int saveValue = fileChooser.showSaveDialog(null);
+
+		if (saveValue == JFileChooser.APPROVE_OPTION) {
+			try {
+
+				// Si la imagen era un .raw la guardo como JPG mandatoriamente,
+				// sino se guarda con el mismo formato que tenia previamente
+				if (imagenAGuardar.getFormato().equals(FormatoDeImagen.RAW)) {
+
+					ImageIO.write(imagenAGuardar.getBufferedImage(), "jpg",
+							new File(fileChooser.getSelectedFile()
+									.getAbsolutePath() + ".jpg"));
+				} else {
+
+					ImageIO.write(imagenAGuardar.getBufferedImage(),
+							imagenAGuardar.getFormato().getNombre(), new File(
+									fileChooser.getSelectedFile()
+											.getAbsolutePath()
+											+ imagenAGuardar.getFormato()
+													.getExtension()));
+				}
+				DialogsHelper.mostarMensaje(contentPane, "Imagen guardada.");
+
+			} catch (IOException exception) {
+
+				exception.printStackTrace();
+				DialogsHelper.mostarMensaje(contentPane,
+						"No se pudo guardar la imagen.", NivelMensaje.ERROR);
+			}
+		}
+	}
+
 	private JFreeChart dibujarHistograma(float[] histograma, JPanel jPanel, Canal colorBarras) {
 
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
